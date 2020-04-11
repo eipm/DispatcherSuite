64495081410bd4b7929081d48367208f88eff954
Manuele Simi <mas2182@med.cornell.edu>
2020-04-11 14:45:10 -0400

    Correct version in About.

--- kafka-service/src/main/java/edu/cornell/eipm/messaging/microservices/kafka/dispatcher/About.java
+++ kafka-service/src/main/java/edu/cornell/eipm/messaging/microservices/kafka/dispatcher/About.java
@@ -3,7 +3,7 @@ package edu.cornell.eipm.messaging.microservices.kafka.dispatcher;
 public class About {
 
     private String component = "Kafka-Dispatcher Service";
-    private String version = "1.1";
+    private String version = "1.1.2";
     private final String content;
 
 
