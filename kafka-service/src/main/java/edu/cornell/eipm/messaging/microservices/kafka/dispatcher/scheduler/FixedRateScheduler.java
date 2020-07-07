4db2733b50ff0ff5d5daf65d2d2b3230c0abe90f
Manuele Simi <mas2182@med.cornell.edu>
2020-07-07 17:52:31 -0400

    Attach the enabling annotation to each scheduler.

--- kafka-service/src/main/java/edu/cornell/eipm/messaging/microservices/kafka/dispatcher/scheduler/FixedRateScheduler.java
+++ kafka-service/src/main/java/edu/cornell/eipm/messaging/microservices/kafka/dispatcher/scheduler/FixedRateScheduler.java
@@ -1,23 +1,17 @@
 package edu.cornell.eipm.messaging.microservices.kafka.dispatcher.scheduler;
 
-import edu.cornell.eipm.messaging.microservices.executors.model.service.Action;
-import edu.cornell.eipm.messaging.microservices.kafka.dispatcher.config.KafkaService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Objects;
+
 
 /**
  * Scheduler with fixed rates for configured jobs.
  *
  * @author Manuele Simi
  */
+@EnableScheduling
 @Configuration
 @Component
 @ConditionalOnProperty(name = "dispatcher.schedulers.fixed.enable")
