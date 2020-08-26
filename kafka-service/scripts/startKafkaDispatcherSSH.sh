94f70afd9f25932948b8a20caf062d74e44cce6a
Manuele Simi <mas2182@med.cornell.edu>
2020-08-26 09:39:23 -0400

    Update start scripts.

--- kafka-service/scripts/startKafkaDispatcherSSH.sh
+++ kafka-service/scripts/startKafkaDispatcherSSH.sh
@@ -7,16 +7,16 @@ if [[ -e .pid ]]; then
     echo "Stop the container first or remove the .pid file if the container is not running."
     exit 1
 fi
-
 mkdir -p ${app_folder}/log/
-chmod -R a+w+r ${app_folder}/log/
-chmod  a+r ${app_folder}/application.yml
-
 name="kd$RANDOM"
 echo "$name" > .pid
 
 nohup docker run -p 9098:8080 --rm --name ${name} \
-    --userns=host \
+    -e HOST_HOSTNAME=$(hostname) \
+    -e HOST_USER=$LOGNAME \
+    -e HOST_USER_ID=$(id -u) \
     -v ${app_folder}/log/:/log/ \
+    -v $HOME/.ssh/:/ssh/:ro \
+    --userns=host \
     -v ${app_folder}/application.yml:/config/application.yml \
     eipm/kafka-dispatcher:1.2.1 &
