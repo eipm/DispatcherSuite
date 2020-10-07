7b921d8ad0a6f41ec84dc70be8f7e38abdd6d3f7
Manuele Simi <mas2182@med.cornell.edu>
2020-10-06 22:55:59 -0400

    Remove Dockerfile (it has been moved to its own repository).

--- kafka-service/buildDockerImage.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-docker_image_name='eipm/kafka-dispatcher'
-docker_image_tag='1.2.1'
-docker build --iidfile ./id.txt .
-docker tag $(cat ./id.txt) ${docker_image_name}:${docker_image_tag}
-docker tag $(cat ./id.txt) ${docker_image_name}:latest
-rm ./id.txt
