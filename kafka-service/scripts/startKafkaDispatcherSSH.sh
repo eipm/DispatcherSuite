b15cbbe37a3b3b13e22190a6f6a6b9f664b9861d
Manuele Simi <mas2182@med.cornell.edu>
2020-11-12 11:45:52 -0500

    Add checks to start scripts.

--- kafka-service/scripts/startKafkaDispatcherSSH.sh
+++ kafka-service/scripts/startKafkaDispatcherSSH.sh
@@ -4,7 +4,7 @@ app_folder=$1
 
 if [[ -z "$app_folder" ]]; then
       echo "ERROR: config folder not set"
-      echo "Usage: startKafkaDispatcher.sh <config folder>"
+      echo "Usage: startKafkaDispatcherSSH.sh <config folder>"
       exit 1
 else
       echo "Using ${app_folder} as config folder"
@@ -15,7 +15,7 @@ if [[ -e .pid ]]; then
     echo "Stop the container first or remove the .pid file if the container is not running."
     exit 1
 fi
-
+mkdir -p ${app_folder}/log/
 name="kd$RANDOM"
 echo "$name" > .pid
 
@@ -23,7 +23,8 @@ nohup docker run -p 9098:8080 --rm --name ${name} \
     -e HOST_HOSTNAME=$(hostname) \
     -e HOST_USER=$LOGNAME \
     -e HOST_USER_ID=$(id -u) \
+    -v ${app_folder}/log/:/log/ \
     -v $HOME/.ssh/:/ssh/:ro \
     --userns=host \
     -v ${app_folder}/application.yml:/config/application.yml \
-    eipm/kafka-dispatcher &
+    eipm/kafka-dispatcher:1.2.1 &
