b0e393cc66cf886dad15cf9fdb85714c9d62b03e
Manuele Simi <mas2182@med.cornell.edu>
2023-04-07 22:48:33 -0400

    Advance Spring Boot to 2.7.10. Increase service version to 1.4.4.

--- kafka-service/runSSLContainer.sh
+++ kafka-service/runSSLContainer.sh
@@ -7,5 +7,5 @@ docker run -p 8443:8443 --rm  \
  -e HOST_USER=$LOGNAME \
  -v "${WORKING_DIR}/application-ssl.yml":/application.yml \
   -v /Users/manuelesimi/EIPM/DispatcherSuite/kafka-service/scripts/kd-keystore.jks:/kd-keystore.jks \
-  cgen/kafka-dispatcher:1.3.2
+  eipm/kafka-dispatcher:1.4.4
 
