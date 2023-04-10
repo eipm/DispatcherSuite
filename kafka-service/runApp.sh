db4836da36632d9ae463ef9bd44f905e100f08e8
Manuele Simi <2822686+manuelesimi@users.noreply.github.com>
2023-04-10 12:13:36 -0400

    Update kafka-service/runApp.sh
    Co-authored-by: Andrea Sboner <asboner@users.noreply.github.com>

--- kafka-service/runApp.sh
+++ kafka-service/runApp.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar target/kafka-service-1.4.4.war -Dspring.config.location=/Users/manuelesimi/Projects/EIPM/DispatcherSuite/kafka-service/application.yml
+java -jar target/kafka-service-1.4.4.war -Dspring.config.location=/path/to/EIPM/DispatcherSuite/kafka-service/application.yml
