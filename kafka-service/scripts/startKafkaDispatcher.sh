f4fcc53a8b4df51fabb05793350c2e827e0cc693
Manuele Simi <2822686+manuelesimi@users.noreply.github.com>
2023-04-11 22:21:03 -0400

    Advance Spring Boot to 2.7.10. Increase service version to 1.4.4. (#23)
    * Advance Spring Boot to 2.7.10. Increase service version to 1.4.4.
    
    * Update kafka-service/runApp.sh
    
    Co-authored-by: Andrea Sboner <asboner@users.noreply.github.com>
    
    * Generalize some paths.
    
    ---------
    
    Co-authored-by: Andrea Sboner <asboner@users.noreply.github.com>

--- kafka-service/scripts/startKafkaDispatcher.sh
+++ kafka-service/scripts/startKafkaDispatcher.sh
@@ -37,4 +37,4 @@ docker run -d --rm --name ${name} \
     -e DISPATCHER_PORT=8080 \
     -v ${app_folder}/log/:/log/ \
     -v ${app_folder}/application.yml:/config/application.yml \
-    cgen/kafka-dispatcher:1.3.0.patched20210202
+    eipm/kafka-dispatcher:1.4.3
